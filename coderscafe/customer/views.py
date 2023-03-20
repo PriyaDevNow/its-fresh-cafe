b899e02a134a19f6c7159b89a48e805de320c1f9
PriyaDevNow <priyawaes@gmail.com>
2023-03-20 11:49:15 +0000

    Initial Commit

---
+++ coderscafe/customer/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from django.views import View
+
+# Create your views here.
+
+class Index(View):
+
+    def get(self, request, *arg, **kwargs):
+        return render(request, 'customer/index.html')
+
+class About(View):
+
+    def get(self, request, *arg, **kwargs):
+        return render(request, 'customer/about.html')
