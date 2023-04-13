0b72ade70dda329aa8e7e2d690e9ef793aa8eb00
PriyaDevNow <priyawaes@gmail.com>
2023-04-13 13:59:55 +0000

    Settings,py changed for Debug & Allowed Host

--- coderscafe/coderscafe/settings.py
+++ coderscafe/coderscafe/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-m&4brnwm^adxu)30x#o49$vf-)srj9@3uiuyf5(=m28phpf^a#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 
 # Application definition
